0e61e72f18272b73a2bbc24c952b4d41aec42eaa
Chen <dehua.chen@sga.pucminas.br>
2016-09-27 17:31:03 -0300

    Incluiu todas as 6 telas. As telas nao possuem nada no momento.

---
+++ app/src/main/java/com/cdh/sms/CtrlMontar.java
@@ -0,0 +1,15 @@
+package com.cdh.sms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CtrlMontar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_montar);
+
+        // @// TODO: 16/9/27 detalhar a tela montar, fragment, etc...
+    }
+}
