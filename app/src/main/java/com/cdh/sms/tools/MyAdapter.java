81c91be9fc39b40590c0320781d4a9499e4234c5
cc <dehua.chen@sga.pucminas.br>
2016-11-20 13:37:43 -0200

    corrigiu erro na tela RecupToken, o detalhe do pedido incompleto.

--- app/src/main/java/com/cdh/sms/tools/MyAdapter.java
+++ app/src/main/java/com/cdh/sms/tools/MyAdapter.java
@@ -1,9 +1,18 @@
 package com.cdh.sms.tools;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TableRow;
+import android.widget.TextView;
 
+import com.cdh.sms.R;
 import com.cdh.sms.model.Sanduiche;
+
+import java.util.List;
 
 /**
  * Created by cc on 16-11-18.
@@ -13,5 +22,31 @@ public class MyAdapter extends ArrayAdapter<Sanduiche> {
 
     public MyAdapter(Context context, int resource) {
         super(context, resource);
+    }
+
+    public MyAdapter(Context context, int resource, List<Sanduiche> items) {
+        super(context, resource, items);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+
+        if (v == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            v = inflater.inflate(R.layout.list_item, null);
+        }
+
+        Sanduiche sanduiche = getItem(position);
+
+
+        if (sanduiche != null) {
+            TextView tt1 = (TextView) v.findViewById(R.id.id);
+            TextView tt2 = (TextView) v.findViewById(R.id.categoryId);
+            TextView tt3 = (TextView) v.findViewById(R.id.description);
+        }
+
+        return v;
     }
 }
